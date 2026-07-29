b09855a1f882014825000cd7295d889a4c562ebf
frankescuela <frankescuela500@gmail.com>
2026-07-28 20:46:56 -0400

    Add files via upload

---
+++ Models/Producto.cs
@@ -0,0 +1,24 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace AppASPCurso.Models
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "La descripción es obligatoria")]
+        public string Descripcion { get; set; }
+
+        [Required(ErrorMessage = "El precio es obligatorio")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "El precio debe ser mayor a 0")]
+        public decimal Precio { get; set; }
+
+        [Required(ErrorMessage = "El stock es obligatorio")]
+        [Range(0, int.MaxValue, ErrorMessage = "El stock no puede ser negativo")]
+        public int stock { get; set; }
+    }
+}
